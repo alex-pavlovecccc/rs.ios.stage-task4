6646a2a9aacebbd9916b8a54a720a8f77d3097e7
alex-pavlovecccc <alex.pavloveccccc@gmail.com>
2021-06-06 22:06:50 +0300

    feat: attempt at solution ex2

--- Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-2/FillWithColor.swift
@@ -2,7 +2,35 @@ import Foundation
 
 final class FillWithColor {
     
+    var bufer: [[Int]] = [[]]
+    
     func fillWithColor(_ image: [[Int]], _ row: Int, _ column: Int, _ newColor: Int) -> [[Int]] {
-        [[]]
+        
+        let m = image.count
+        let n = image[0].count
+        
+        if (row < 0 && column < 0 && row >= m && column >= n && image[row][column] == newColor) {
+            return image
+        }
+        
+        bufer = image
+        
+        draw(row, column, newColor)
+        return bufer
+    }
+    
+    func draw(_ i: Int, _ j: Int, _ newColor: Int){
+        
+        if (i >= 0 && i < bufer.count && j >= 0 && j < bufer[i].count && bufer[i][j] != newColor) {
+            
+            bufer[i][j] = newColor
+            
+            
+            draw(i, j + 1, newColor)
+            draw(i + 1, j, newColor)
+            draw(i, j - 1, newColor)
+            draw(i - 1, j, newColor)
+        }
     }
 }
+
