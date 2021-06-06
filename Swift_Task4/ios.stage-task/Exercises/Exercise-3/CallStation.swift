97db4ebf14a6875572502df515c63b7e4b2a6447
alex-pavlovecccc <alex.pavloveccccc@gmail.com>
2021-06-06 22:06:40 +0300

    feat: attempt at solution ex3

--- Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
@@ -1,30 +1,56 @@
 import Foundation
 
-final class CallStation { }
+final class CallStation {
+    var baseUser: [User] = []
+    var callArray: [Call] = []
+    
+}
 
 extension CallStation: Station {
     func users() -> [User] {
-        []
+        return baseUser
     }
     
     func add(user: User) {
-
+        if !baseUser.contains(user) {
+            baseUser.append(user)}
     }
     
     func remove(user: User) {
-
+        if !baseUser.contains(user) {
+            baseUser.remove(at: baseUser.firstIndex(of: user) ?? 0) }
     }
     
     func execute(action: CallAction) -> CallID? {
-        nil
+        
+        let call: Call
+        let uuid: CallID = UUID()
+        
+        switch action {
+        case .start(from: let from, to: let to):
+            
+            if !baseUser.contains(from) && !baseUser.contains(to) {
+                return nil
+            } else if !baseUser.contains(to) {
+                call = Call(id: uuid, incomingUser: from, outgoingUser: to, status: .ended(reason: .error))
+                return call.id
+            }
+            
+//            let call = Call(id: UUID.init(), incomingUser: from, outgoingUser: to, status: .calling)
+//                            callArray.append(call)
+            
+        default:
+            return nil
+        }
+        return nil
     }
     
     func calls() -> [Call] {
-        []
+        return callArray
     }
     
     func calls(user: User) -> [Call] {
-        []
+        return callArray
     }
     
     func call(id: CallID) -> Call? {
